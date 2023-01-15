36667de5eff9db86286a2da9ad6e49c1ccbbac69
baojie <1373605496@qq.com>
2023-01-15 11:25:12 +0800

    初始化项目

---
+++ src/js/flagMap.js
@@ -0,0 +1,20 @@
+const arry = ['hello', ['1', 2, '3'], [4, 5, ['js', 'vue', ['vue', 'vuex']]]]
+flatArry = (arr) => {
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result.push(...flatArry(arr[i]))
+    } else {
+      result.push(arr[i])
+    }
+  }
+  return result
+}
+console.log(...flatArry(arry))
+const obj = {
+  name: 'hello',
+  cast: '123',
+  majer: {
+    openid: 'r'
+  }
+}
---
+++ src/js/url.js
@@ -0,0 +1,8 @@
+// const getParamByUrl = (key) => {
+//   const url = new URL(location.href)
+//   return url.searchParams.get(key)
+// }
+
+// const url = getParamByUrl(
+//   'https://www.bilibili.com/video/BV1Xa411z7Hc/?p=10&spm_id_from=pageDriver&vd_source=8e7775e8f80311672d92dcbdaba484d9'
+// )
