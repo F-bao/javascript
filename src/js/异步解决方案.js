c6ed109a46cbdc82c35d54f13208afe12049d49e
baojie <1373605496@qq.com>
2023-02-13 08:56:43 +0800

    异步解决方案

---
+++ src/js/异步解决方案.js
@@ -0,0 +1,38 @@
+// function fun() {
+//   let res
+//   setTimeout(() => {
+//     res = 'nihao'
+//   }, 100)
+//   console.log('res==>', res)
+// }
+//
+// fun()
+
+//callback 回调函数
+// function getData(callback) {
+//   let res
+//   setTimeout(() => {
+//     callback('hello')
+//   }, 100)
+//   console.log('res==>', res)
+// }
+//
+// getData((res) => {
+//   console.log('res===>', res)
+// })
+
+
+//promise
+
+function promiseFun() {
+  return new Promise((resolve, reject) => {
+    // resolve('success')
+    reject('fail')
+  })
+}
+
+promiseFun().then(res => {
+  console.log('res===>', res)
+}).catch(reason => {
+  console.log(reason)
+})
