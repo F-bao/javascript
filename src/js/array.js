c67ec9588a2eeb2b98ed03c745df1775cf239f28
baojie <1373605496@qq.com>
2023-02-28 09:42:32 +0800

    统计字符串重复出现次数

--- src/js/array.js
+++ src/js/array.js
@@ -1,3 +1,24 @@
-let array = [1, 2, 3]
-let num = array.push(4)
-console.log(num)
+// for (let i = 0; i < 5; i++) {
+//   setTimeout(() => {
+//     console.log(i)
+//   },1000)
+// }
+//统计字符串重复出现次数
+let array = [...'agkgjihkjkgsdosadcn'].reduce((a, b) => {
+
+  a[b] ? a[b]++ : a[b] = 1
+  return a
+
+}, {})
+console.log(array)
+
+let arr = [...'agkgjihkjkgsdosadcn']
+let obj = {}
+for (let i = 0; i < arr.length; i++) {
+  obj[arr[i]] = (obj[arr[i]] + 1) || 1
+  if (obj[arr[i]] === 2) {
+    console.log(arr[i])
+    break
+  }
+}
+console.log(obj)
