8b8f91049cc9a7aba9d0b163de7993b7775563aa
baojie <1373605496@qq.com>
2023-07-03 19:42:36 +0800

    Revert "feat:修改配置格式化配置，测试Element-plus 类型推断"
    This reverts commit b30c620935a3f2f812344da3d55bf2d230b0a439.

--- src/ts/test.ts
+++ src/ts/test.ts
@@ -1,60 +1,30 @@
 const a: number = 100;
 
 let fun = (): [] => {
-	return [];
+  return [];
 };
 function func(): [] {
-	return [];
+  return [];
 }
 
 let fun2 = (a: number, b: number): number => a + b;
 
 export function reverseNumber(num: number): number {
-	let res = Number(num.toString().split("").reverse().join(""));
-	return res;
+  let res = Number(num.toString().split("").reverse().join(""));
+  return res;
 }
 
 export function reverseString(str: string): string {
-	return str.split("").reverse().join("");
+  return str.split("").reverse().join("");
 }
 
 export interface IPerson {
-	readonly name: string;
-	age: number;
-	career?: string;
-	isNickLooking: boolean;
+  readonly name: string;
+  age: number;
+  career?: string;
+  isNickLooking: boolean;
 }
 
 export interface IFun {
-	(x: number, y: number): number;
+  (x: number, y: number): number;
 }
-
-export function debounce(func: Function, wait: number, immediate: boolean = false) {
-	let timer: any;
-
-	return function (this: any, ...args: any[]) {
-		const context = this;
-
-		clearTimeout(timer);
-
-		timer = setTimeout(() => {
-			timer = null;
-			if (!immediate) {
-				func.apply(context, args);
-			}
-		}, wait);
-
-		if (immediate && !timer) {
-			func.apply(context, args);
-		}
-	};
-}
-
-let f = debounce(
-	function () {
-		console.log(222);
-	},
-	3000,
-	true
-);
-f();
